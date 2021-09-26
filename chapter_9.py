ecfe0638dbdfb99cbd6e42efc99787c60bc703ad
BianLong0618 <bianlong0618@qq.com>
2021-09-26 11:02:40 +0800

    Add the code for the k closest elements to median in 9.3-7 P223.

--- chapter_9.py
+++ chapter_9.py
@@ -6,7 +6,6 @@ But this technique is of theoretical interest in relating selection and sorting 
 import random
 import math
 
-
 """
 Get the minimum and the maximum elements of A simultaneously.
 First we compare each elements in pairs, 
@@ -16,7 +15,9 @@ search the possible maximum in the larger elements in all pairs.
 Input: list A.
 Output: the minimum & maximum elements in A.
 """
-def min_max( A ):
+
+
+def min_max(A):
     n = len(A)
 
     #  Comparing in pairs.
@@ -39,20 +40,21 @@ def min_max( A ):
     return Amin, Amax
 
 
+#  The classic partition in Quicksort.
+def partition(A, p, r):
+    j = p - 1
+    for i in range(p, r):
+        if A[i] <= A[r]:
+            j += 1
+            A[j], A[i] = A[i], A[j]
+    A[j + 1], A[r] = A[r], A[j + 1]
+    return j + 1
+
+
 #  Randomized partition in Quicksort.
 def random_partition(A, p, r):
-
-    #  The classic partition in Quicksort.
-    def partition(A, p, r):
-        j = p - 1
-        for i in range(p, r):
-            if A[i] <= A[r]:
-                j += 1
-                A[j], A[i] = A[i], A[j]
-        A[j + 1], A[r] = A[r], A[j + 1]
-        return j + 1
     #  Choosing the pivot randomly.
-    j = random.randint( p, r )
+    j = random.randint(p, r)
     A[j], A[r] = A[r], A[j]
     return partition(A, p, r)
 
@@ -62,8 +64,9 @@ Select the ith largest element in array A recursively.
 Input @para: list A, start index p and end index r, i means ith largest element.
 Output: the ith largest element in array A[p,...,r] and its index.
 """
-def select_recur( A, p, r, i ):
 
+
+def select_recur(A, p, r, i):
     #  Partition list A randomly, then search ith element in which subarray of A.
     q = random_partition(A, p, r)
     #  Transfer index q to kth.
@@ -75,11 +78,11 @@ def select_recur( A, p, r, i ):
         return A[q], q
     #  ith element must be in the left subarray.
     if i < k:
-        return select_recur( A, p, q - 1, i)
+        return select_recur(A, p, q - 1, i)
     #  when i > k, ith element must be in the right subarray.
     #  Notice, when go to the right subarray, i need to do some computation.
     else:
-        return select_recur( A, q + 1, r, i - k)
+        return select_recur(A, q + 1, r, i - k)
 
 
 """
@@ -87,8 +90,9 @@ Select the ith largest element in array A iteratively.
 Input @para: list A, i means ith largest element.
 Output: the ith largest element in array A[p,...,r]
 """
-def select_iter(A, i):
 
+
+def select_iter(A, i):
     n = len(A)
     p = 0
     r = n - 1
@@ -117,31 +121,32 @@ divide the sorted set into k equal-sized sets (to within 1).
 Input @para: list A, p & r are indices of A that A[p,...,r], k means split the array into k segments.
 Output @para: the k - 1 quantiles.
 """
-def kquantiles( A, p, r, k ):
 
+
+def kquantiles(A, p, r, k):
     #  Set sentinel.
     assert k > 1, "It's meaningless otherwise."
 
     n = r - p + 1
     #  Base case 1: just return the median.
     if k == 2:
-        ans = select_recur( A, p, r, math.ceil(n / 2) )
+        ans = select_recur(A, p, r, math.ceil(n / 2))
         return ans[0]
     #  Base case 2: return the three equal points.
     if k == 3:
         ans1 = select_recur(A, p, r, round(n / 3))
         ans2 = select_recur(A, p, r, round(n / 3) * 2)
-        return [ ans1[0], ans2[0] ]
+        return [ans1[0], ans2[0]]
 
     #  When k is an even number.
     if k % 2 == 0:
         #  We find the median and its index first.
-        q_value, q = select_recur( A, p, r, math.ceil( n / 2 ) )
+        q_value, q = select_recur(A, p, r, math.ceil(n / 2))
 
         #  Then we go to the subarray to find k / 2 quantiles recursively.
         #  k / 2 must be an integer as n is an even number.
-        t1 = kquantiles( A, p, q - 1, k / 2 )
-        t2 = kquantiles(A, q + 1, r, k / 2 )
+        t1 = kquantiles(A, p, q - 1, k / 2)
+        t2 = kquantiles(A, q + 1, r, k / 2)
         return [t1, q_value, t2]
 
     #  When k is an odd number.
@@ -149,21 +154,40 @@ def kquantiles( A, p, r, k ):
         #  We find the middle two quantiles and their indices first.
         pivot = k - 1 / 2
         pivot = n / k
-        q1_value, q1 = select_recur(A, p, r, pivot * n / k )
-        q2_value, q2 = select_recur(A, p, r, (pivot + 1 ) * n / k )
+        q1_value, q1 = select_recur(A, p, r, pivot * n / k)
+        q2_value, q2 = select_recur(A, p, r, (pivot + 1) * n / k)
 
         #  Then we go to the subarray to find k - 1 / 2 quantiles recursively.
         #  k - 1 / 2 must be an integer as n is an even number.
-        t1 = kquantiles( A, p, q1 - 1, (k - 1) / 2)
+        t1 = kquantiles(A, p, q1 - 1, (k - 1) / 2)
         t2 = kquantiles(A, q2 + 1, r, (k - 1) / 2)
         return [t1, q1_value, q2_value, t2]
 
 
+"""
+Find the closest k elements around median inclusively of array A.
+Input @para: array A and parameter k.
+Output: the closest k elements.
+"""
+def kclosest( A, k ):
+
+    #  Find the median in A first.
+    n = len(A)
+    median, q = select_recur(A, 0, n - 1, math.ceil( n / 2))  # index q is useless here.
+
+    #  Get the delta of each elements with median.
+    delta = [A[i] - median for i in range(n)]
+    #  We find the first k minimum elements in delta.
+    q_left, index = select_recur(delta, 0, n - 1, max(math.ceil( n / 2 ) - k // 2, 1) )  # index is useless here.
+    q_right, index = select_recur(delta, 0, n - 1, min(math.ceil( n / 2 ) + k // 2, n - 1) )  # index is useless here.
+
+    #  Search the first k minimum elements in A and return.
+    res = [ A[i] for i in range(n) if A[i] >= q_left + median and A[i] <= q_right + median ]
+    return res
 
 
 #  Drive code
 if __name__ == "__main__":
-
     #  Test data collection.
     A0 = [2, 5, 3, 0, 2, 3, 0, 3]
     A1 = [6, 0, 2, 0, 1, 3, 4, 6, 1, 3, 2]
@@ -173,12 +197,13 @@ if __name__ == "__main__":
     A6 = [5, 10, 7, 2, 3, 1, 4, 9, 8, 6]
     A7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
     #  Test for the simultaneous minimum and maximum algorithm in P214.
-    print("Test for the simultaneous minimum and maximum algorithm in P214: ", min_max( A3 ), '\n')
+    print("Test for the simultaneous minimum and maximum algorithm in P214: ", min_max(A3), '\n')
     #  Test for recursive select in P216.
-    print("Test for recursive select in P216: ", select_recur( A2[:], 0, len(A2) - 1, 5 ) )
+    print("Test for recursive select in P216: ", select_recur(A2[:], 0, len(A2) - 1, 5))
     #  Test for iterative select in 9.2-3 P219.
-    print("Test for iterative select in 9.2-3 P219: ", select_iter( A2[:], 5 ), '\n' )
+    print("Test for iterative select in 9.2-3 P219: ", select_iter(A2[:], 5), '\n')
     #  Test for the k-quantiles in 9.3-6 P223.
-    print("Test for the k-quantiles in 9.3-6 P223: ", kquantiles( A7[:], 0, len(A7) - 1, 5 ), '\n' )
+    print("Test for the k-quantiles in 9.3-6 P223: ", kquantiles(A7[:], 0, len(A7) - 1, 5), '\n')
+    #  Test for the k closest elements to median in 9.3-7 P223.
+    print("Test for the k closest to median func in 9.3-7 P223: ", kclosest(A6[:], 4), '\n')
